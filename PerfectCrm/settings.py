38fceae1a61a6da3aca1d4248f622e89249d0cd0
Winter Solstice Festival <32798102+diondigi@users.noreply.github.com>
2018-02-10 19:26:12 +0800

    Update settings.py

--- PerfectCrm/settings.py
+++ PerfectCrm/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE':'django.db.backends.mysql',
         'NAME':'mycrm',
         'USER': 'root',
-        'PASSWORD': '199033',
+        'PASSWORD': 'xxxxxx',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
